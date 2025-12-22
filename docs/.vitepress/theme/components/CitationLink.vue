01e60e67e1c4b57198ae9327b848b0d3490ce740
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-12-22 16:38:44 +0330

    Add CitationLink component for external links

---
+++ docs/.vitepress/theme/components/CitationLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="url" class="citation-link">
+    <img :src="faviconUrl" alt="favicon" class="favicon-icon" />
+    {{ label || 'منبع' }}
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  url: String,
+  label: String
+})
+
+const faviconUrl = `https://www.google.com/s2/favicons?domain=${new URL(props.url).hostname}`
+</script>
+
+<style>
+.citation-link {
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  color: blue;
+}
+.favicon-icon {
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+}
+</style>
