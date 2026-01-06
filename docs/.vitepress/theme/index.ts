f9c5c9d31a63c83d51724132b67c84319e6fabbc
Diana <135276904+Diana-Cl@users.noreply.github.com>
2026-01-06 12:00:26 +0330

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,29 +1,15 @@
-import DefaultTheme from 'vitepress/theme';
-import type { Theme } from 'vitepress';
-import { useRoute } from 'vitepress';
-import './styles.css';
-import './vars.css';
-import 'viewerjs/dist/viewer.min.css';
-import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css';
-import './custom.css';
-import Ltr from './components/Ltr.vue';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
-import { NolebaseInlineLinkPreview } from '@nolebase/vitepress-plugin-inline-link-preview/client';
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import './styles.css'
+
 export default {
   extends: DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp?.(ctx);
-
-    ctx.app.component('vImageViewer', vImageViewer);
-    enhanceAppWithTabs(ctx.app);
-    ctx.app.component('NolebaseInlineLinkPreview', NolebaseInlineLinkPreview);
-    ctx.app.component('CitationLink', CitationLink);
-    ctx.app.component('Ltr', Ltr);
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default
+    })
   },
-  setup() {
-    const route = useRoute();
-    imageViewer(route);
+  enhanceApp({ app, router, siteData }) {
   }
-} satisfies Theme;
+} satisfies Theme
